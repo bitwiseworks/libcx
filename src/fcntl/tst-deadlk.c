e8770bde1fd5acb093191bbacb68536c0d2a132c
Dmitriy Kuminov <coding@dmik.org>
2016-06-07 03:23:48 +0400

    tst-deadlk.c: Fix copyright notice.

--- src/fcntl/tst-deadlk.c
+++ src/fcntl/tst-deadlk.c
@@ -1,14 +1,14 @@
-/* Copyright (C) 2002-2016 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-   Contributed by Ulrich Drepper <drepper@redhat.com>, 2002.
+/* Copyright (C) 2016 bww bitwise works GmbH.
+   This file is part of the kLIBC Extension Library.
+   Authored by Dmitry Kuminov <coding@dmik.org>, 2016.
 
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
+   The kLIBC Extension Library is free software; you can redistribute it
+   and/or modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
 
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   The kLIBC Extension Library is distributed in the hope that it will be
+   useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
 
