9a33b349bf32d838afc990d7ba6a4c4947048cb1
Dmitriy Kuminov <coding@dmik.org>
2018-08-30 23:32:47 +0300

    net: Remove ifndef guards around getaddrinfo constants.
    These guards allow to redefine constants on the application level which is not right because getaddrinfo will not pick these redefinitions up and just go out of sync.
    
    Needed for #37.

--- src/net/libcx/net.h
+++ src/net/libcx/net.h
@@ -34,68 +34,28 @@
 #include <stddef.h>
 
 /* GAI error codes */
-#ifndef EAI_BADFLAGS
 #define EAI_BADFLAGS    -1
-#endif
-#ifndef EAI_NONAME
 #define EAI_NONAME      -2
-#endif
-#ifndef EAI_AGAIN
 #define EAI_AGAIN       -3
-#endif
-#ifndef EAI_FAIL
 #define EAI_FAIL        -4
-#endif
-#ifndef EAI_NODATA
 #define EAI_NODATA      -5
-#endif
-#ifndef EAI_FAMILY
 #define EAI_FAMILY      -6
-#endif
-#ifndef EAI_SOCKTYPE
 #define EAI_SOCKTYPE    -7
-#endif
-#ifndef EAI_SERVICE
 #define EAI_SERVICE     -8
-#endif
-#ifndef EAI_ADDRFAMILY
 #define EAI_ADDRFAMILY  -9
-#endif
-#ifndef EAI_MEMORY
 #define EAI_MEMORY      -10
-#endif
-#ifndef EAI_OVERFLOW
 #define EAI_OVERFLOW    -11
-#endif
-#ifndef EAI_SYSTEM
 #define EAI_SYSTEM      -12
-#endif
 
-#ifndef NI_NUMERICHOST
 #define NI_NUMERICHOST 0x01
-#endif
-#ifndef NI_NUMERICSERV
 #define NI_NUMERICSERV 0x02
-#endif
-#ifndef NI_NOFQDN
 #define NI_NOFQDN      0x04
-#endif
-#ifndef NI_NAMEREQD
 #define NI_NAMEREQD    0x08
-#endif
-#ifndef NI_DGRAM
 #define NI_DGRAM       0x10
-#endif
 
-#ifndef AI_PASSIVE
 #define AI_PASSIVE     1
-#endif
-#ifndef AI_CANONNAME
 #define AI_CANONNAME   2
-#endif
-#ifndef AI_NUMERICHOST
 #define AI_NUMERICHOST 4
-#endif
 
 typedef int socklen_t;
 
