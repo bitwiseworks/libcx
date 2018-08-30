48deaef240b3d239b962ab714abae885cec1d624
Dmitriy Kuminov <coding@dmik.org>
2018-08-30 23:19:09 +0300

    net: Start off network extensions.
    This commit adds a header that will collect all extensions. Many definitions from this header belong to some standard headers but since kLIBC already provides these headers, we don't have control over them from LIBCx.
    
    Needed for #37.

---
+++ src/net/libcx/net.h
@@ -0,0 +1,25 @@
+/*
+ * network extensions for kLIBC.
+ * Copyright (C) 2018 bww bitwise works GmbH.
+ * This file is part of the kLIBC Extension Library.
+ * Authored by Dmitry Kuminov <coding@dmik.org>, 2018.
+ *
+ * The kLIBC Extension Library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The kLIBC Extension Library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with the GNU C Library; if not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBCX_NET_H
+#define LIBCX_NET_H
+
+#endif // LIBCX_NET_H
