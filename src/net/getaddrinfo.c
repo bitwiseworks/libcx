6b0c6427e12fecbd8a9502b01d46154dff84462f
Dmitriy Kuminov <coding@dmik.org>
2018-09-05 02:13:38 +0300

    net: Do better error reporting in gai_strerror.
    Needed for #37.

--- src/net/getaddrinfo.c
+++ src/net/getaddrinfo.c
@@ -343,60 +343,37 @@ void rep_freeaddrinfo(struct addrinfo *res)
 
 const char *rep_gai_strerror(int errcode)
 {
-#ifdef HAVE_HSTRERROR
-	int			hcode;
+	/* This is a mix of Samba and VLC implementations */
 
 	switch (errcode)
 	{
-		case EAI_NONAME:
-			hcode = HOST_NOT_FOUND;
-			break;
-		case EAI_AGAIN:
-			hcode = TRY_AGAIN;
-			break;
-		case EAI_FAIL:
-		default:
-			hcode = NO_RECOVERY;
-			break;
-	}
-
-	return hstrerror(hcode);
-#else							/* !HAVE_HSTRERROR */
-
-	switch (errcode)
-	{
+		case EAI_BADFLAGS:
+			return "Invalid argument";
 		case EAI_NONAME:
 			return "Unknown host";
 		case EAI_AGAIN:
-			return "Host name lookup failure";
-#ifdef EAI_BADFLAGS
-		case EAI_BADFLAGS:
-			return "Invalid argument";
-#endif
-#ifdef EAI_FAMILY
-		case EAI_FAMILY:
-			return "Address family not supported";
-#endif
-#ifdef EAI_MEMORY
-		case EAI_MEMORY:
-			return "Not enough memory";
-#endif
-#ifdef EAI_NODATA
+			return "Temporary name service failure";
+		case EAI_FAIL:
+			return "Non-recoverable name service failure";
 		case EAI_NODATA:
 			return "No host data of that type was found";
-#endif
-#ifdef EAI_SERVICE
-		case EAI_SERVICE:
-			return "Class type not found";
-#endif
-#ifdef EAI_SOCKTYPE
+		case EAI_FAMILY:
+			return "Address family not supported";
 		case EAI_SOCKTYPE:
 			return "Socket type not supported";
-#endif
+		case EAI_SERVICE:
+			return "Class type not found";
+		case EAI_ADDRFAMILY:
+			return "Unavailable address family for host name";
+		case EAI_MEMORY:
+			return "Not enough memory";
+		case EAI_OVERFLOW:
+			return "Buffer overflow";
+		case EAI_SYSTEM:
+			return "System error";
 		default:
 			return "Unknown server error";
 	}
-#endif   /* HAVE_HSTRERROR */
 }
 
 static int gethostnameinfo(const struct sockaddr *sa,
